db16c7e290385b63b27c30ec7f1f5b2d05216fb1
Andrew Barr <a.g.barr@sron.nl>
2023-12-11 15:16:19 +0100

    Add parameterisation mean, min, max test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,29 +29,30 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
-@pytest.mark.paramterize(
+@pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0,0], [0,0], [0,0] ], [0,0]),
-        ([ [5,6], [3,2], [1,2] ], [5,6]),
+        ([ [0, 0, 0], [0, 0, 0], [0, 0, 0] ], [0, 0, 0]),
+        ([ [4, 2, 5], [1, 6, 2], [4, 1, 9] ], [4, 6, 9]),
+        ([ [4, -2, 5], [1, -6, 2], [-4, -1, 9] ], [4, -1, 9]),
     ])
 def test_daily_max(test, expected):
-    """Test max function works for array of zeroes and positive integers."""
+    """Test max function works for zeroes, positive integers, mix of positive/negative integers."""
     from inflammation.models import daily_max
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
-    npt.assert_array_equal(daily_max(np.array(test)),np.array(expected))
 
-
-@pytest.mark.paramterize(
+@pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0,0], [0,0], [0,0] ], [0,0]),
-        ([ [5,6], [3,2], [1,2] ], [1,2]),
+        ([ [0, 0, 0], [0, 0, 0], [0, 0, 0] ], [0, 0, 0]),
+        ([ [4, 2, 5], [1, 6, 2], [4, 1, 9] ], [1, 1, 2]),
+        ([ [4, -2, 5], [1, -6, 2], [-4, -1, 9] ], [-4, -6, 2]),
     ])
 def test_daily_min(test, expected):
-    """Test max function works for array of zeroes and positive integers."""
+    """Test min function works for zeroes, positive integers, mix of positive/negative integers."""
     from inflammation.models import daily_min
-    npt.assert_array_equal(daily_min(np.array(test)),np.array(expected))
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
 
 
