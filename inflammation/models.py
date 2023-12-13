e233522cae37f0fbf1c927603c84e98bc8666988
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-13 11:24:53 +0100

    Added double-spacing between functions

--- inflammation/models.py
+++ inflammation/models.py
@@ -36,9 +36,11 @@ def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array."""
     return np.min(data, axis=0)
 
+
 def daily_std(data):
     """Calculate the daily standard deviation of a 2D inflammation array"""
     return np.std(data, axis=0)
+
 
 def daily_above_threshold(patient_num, data, threshold):
     binary = map(lambda x: x>threshold, data[patient_num])
